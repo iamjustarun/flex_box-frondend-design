43e4929267bee68850dc6305e62a97b6c1e0d001
Arun Chapagain <chaparun2074@gmail.com>
2023-01-07 12:51:20 +0545

    first commit

---
+++ lesson/first.js
@@ -0,0 +1,24 @@
+// function alertMe(){
+//   alert('Hi!');
+// }
+
+// const btn=document.querySelector('button');
+// function addParagraph(){
+//   console.log('button is working');
+// }
+// // btn.addEventListener('click',addParagraph)
+// btn.onclick=addParagraph;
+
+
+// const btn=document.querySelector('button');
+// function addParagraph(){
+// const randomNum=Math.floor(Math.random() * 100)
+// const pContent='The random number is: ' + randomNum;
+// //   creating the paragraph element
+//   const newElement =document.createElement('p');
+//   newElement.textContent=pContent;
+//   document.body.appendChild(newElement);
+//   console.log(pContent);
+// }
+// btn.onclick=addParagraph;
+
